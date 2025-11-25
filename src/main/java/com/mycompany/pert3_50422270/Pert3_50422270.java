63b4670b97ab3b3395dcba8c2720b3c21473579a
unknown <arumsipayung2@gmail.com>
2025-11-25 18:59:49 +0700

    First Commit

---
+++ src/main/java/com/mycompany/pert3_50422270/Pert3_50422270.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert3_50422270;
+
+/**
+ *
+ * @author AGS
+ */
+public class Pert3_50422270 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
